07ba4f9f6c2bb70d7dd1f03f357b913dda19f03b
Anna Merkul <amerkul@solvd.com>
2023-11-14 12:44:41 +0300

    Create test project

---
+++ src/main/java/com/example/cicd/controller/ControllerExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.cicd.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception ex) {
+        return ex.getMessage();
+    }
+
+}
