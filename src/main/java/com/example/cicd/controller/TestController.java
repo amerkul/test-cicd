d0b25eac229c4b249394291bf6c205329d3f28c0
Anna Merkul <amerkul@solvd.com>
2023-11-16 12:03:58 +0300

    Message 5

--- src/main/java/com/example/cicd/controller/TestController.java
+++ src/main/java/com/example/cicd/controller/TestController.java
@@ -25,4 +25,11 @@ public class TestController {
         return "Test message 11";
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/message5", produces = "application/json")
+    public String getMessage5(){
+        return "Test message 11";
+    }
+
+
 }
