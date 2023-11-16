4a501c4f2633e3c87189ba082fa5d091e40b84e3
Anna Merkul <amerkul@solvd.com>
2023-11-16 12:42:56 +0300

    1

--- src/main/java/com/example/cicd/controller/TestController.java
+++ src/main/java/com/example/cicd/controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/message1", produces = "application/json")
+    @GetMapping(value = "/message2", produces = "application/json")
     public String getMessage(){
         return "Test message 456";
     }
